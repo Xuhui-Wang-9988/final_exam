b5b8435ef5dcb1636b5ff6f95837b0cb0bf817b8
ZeruelR <a1809872@student.adelaide.edu.au>
2022-06-17 03:27:38 +0800

    1

---
+++ public/javascripts/home.js
@@ -0,0 +1,14 @@
+function Sign_up() {
+    window.location.href = "/home.html";
+}
+
+function Book_room() {
+    window.location.href = "/book.html";
+}
+
+function Check_history() {
+    window.location.href = "/recent.html";
+}
+
+
+
